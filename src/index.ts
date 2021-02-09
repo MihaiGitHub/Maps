1297affdfb4a6ce28c999b2bec915897742aefc8
Mihai Smarandache <mihai.sanfran@gmail.com>
2021-02-08 21:43:54 -0700

    Display map on screen

--- src/index.ts
+++ src/index.ts
@@ -9,4 +9,10 @@ const company = new Company();
 
 console.log(company);
 
-new google.maps.Map(document.getElementById("map"));
+new google.maps.Map(document.getElementById("map"), {
+  zoom: 1,
+  center: {
+    lat: 0,
+    lng: 0,
+  },
+});
